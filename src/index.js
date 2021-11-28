38c20e71bc66e75422f2cab523863446881b9872
Ella Tanttu <ellajtanttu@gmail.com>
2021-11-28 09:38:34 -0800

    add asset and update js files for setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import { DogMoods } from './project.js';
+import CurrencyService from './js/currency-service';
