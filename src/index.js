fc7693ad787084091f059c308a0ad0cf4da35487
Ella Tanttu <ellajtanttu@gmail.com>
2021-11-28 17:46:19 -0800

    add conditional to present various errors

--- src/index.js
+++ src/index.js
@@ -10,28 +10,60 @@ function clearPrevious() {
   $("#errorMessage").text("");
 }
 
-function showResults(returnedInfo, number, curName) {
+function showResults(returnedInfo, usdNumber, curName) {
   if (returnedInfo.conversion_rates) {
-    console.log(`second time currencyName is ${curName}`);
-    console.log(`rate object at curName ${returnedInfo.conversion_rates}`);
-    $("#conversionResult").text(`${number} dollars is equal to ${parseFloat(returnedInfo.conversion_rates[curName] * number).toFixed(2)} in ${curName}.`);
+    $("#conversionResult").text(`${usdNumber} USD is equal to ${parseFloat(returnedInfo.conversion_rates[curName] * usdNumber).toFixed(2)} ${curName}.`);
   } else {
     $("#errorMessage").text(returnedInfo);
   }
 }
 
+// function showResults(returnedInfo, usdNumber, curName) {
+//   console.log(returnedInfo);
+//   console.log(returnedInfo.result);
+//   console.log(returnedInfo.conversion_rates);
+//   console.log(typeof usdNumber);
+//   console.log(curName);
+//   if (typeof usdNumber !== Number) {
+//     console.log("not a number er er er");
+//   } else if (returnedInfo.conversion_rates["USD"] === 1) {
+//     $("#conversionResult").text(`${usdNumber} USD is equal to ${parseFloat(returnedInfo.conversion_rates[curName] * usdNumber).toFixed(2)} ${curName}.`);
+//   } else {
+//     $("#errorMessage").text(returnedInfo);
+//   }
+// }
+
+// function showResults(returnedInfo, usdNumber, curName) {
+//   if (returnedInfo.conversion_rates) {
+//     console.log(typeof usdNumber);
+//     console.log(typeof (returnedInfo.conversion_rates[curName]));
+//     if ((typeof (returnedInfo.conversion_rates[curName]) === Number) && (typeof usdNumber === Number)) {
+//       $("#conversionResult").text(`${usdNumber} USD is equal to ${parseFloat(returnedInfo.conversion_rates[curName] * usdNumber).toFixed(2)} ${curName}.`);
+//     } else /* if (typeof usdNumber !== Number) */ {
+//       console.log("catch all");
+//     }
+//   } else {
+//     $("#errorMessage").text(returnedInfo);
+//   }
+// }
+
+
 async function showMeTheData(number, curName) {
-  const response = await CurrencyService.getConversions();
-  showResults(response, number, curName);
+  console.log(typeof number);
+  if ((!isNaN(number)) && (curName !== "CLICK PROMPT")){
+    const response = await CurrencyService.getConversions();
+    showResults(response, number, curName);
+  } else {
+    $("#errorMessage").text("Please enter a number and select a currency");
+  }
 }
 
 $(document).ready(function() {
   $('#userButton').click(function() {
     event.preventDefault();
-    const userNumber = $('#conAmount').val();
+    const userNumber = parseInt($('#conAmount').val());
 
     const currencyName = $('#toCurrency').val();
-    console.log(`currencyName is ${currencyName}`);
 
     clearPrevious();
     showMeTheData(userNumber, currencyName);
